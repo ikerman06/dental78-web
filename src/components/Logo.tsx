4b623318664d49ddd186ce7789a564faf6fce50d
Iker <ikerperezpozo2006@gmail.com>
2026-06-08 21:42:28 +0200

    fix: Logo acepta prop color para compilar en Netlify

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,9 @@
 interface LogoProps {
   size?: number;
+  color?: string;
 }
 
-export default function Logo({ size = 40 }: LogoProps) {
+export default function Logo({ size = 40, color = "#777777" }: LogoProps) {
   return (
     <img
       src="/LOGO DENTAL78.png"
